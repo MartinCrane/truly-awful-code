185d89ef27fd2c5034870dac58d193bbde4fce2a
Steven Nunez <steven.nunez@gmail.com>
2017-04-25 08:07:10 -0400

    More complication

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,28 +2,36 @@ class VotesController < ApplicationController
   before_action :authenticate
   def upvote
     @post = Post.find(params[:id])
-    vote = @post.votes.create(account: current_account, vote: "upvote")
-    respond_to do |format|
-      format.js do
-        if vote.save
-          render 'disable_voting'
-        else
-          render 'error_saving_upvote'
-        end
+    vote = @post.votes.new(account: current_account, vote: "upvote")
+    if vote.save
+      redirect_to root_path, notice: "Your upvote has been registered"
+    else
+      existing_vote = @post.votes.find_by(account: current_account)
+      if existing_vote.vote == "upvote"
+        existing_vote.destroy
+        redirect_to root_path, notice: "We've Removed your upvote"
+      else
+        existing_vote.destroy
+        @post.votes.create(account: current_account, vote: "upvote")
+        redirect_to root_path, notice: "Your upvote has been registered"
       end
     end
   end
 
   def downvote
     @post = Post.find(params[:id])
-    vote = @post.votes.create(account: current_account, vote: "downvote")
-    respond_to do |format|
-      format.js do
-        if vote.save
-          render 'disable_voting'
-        else
-          render 'error_saving_upvote'
-        end
+    vote = @post.votes.new(account: current_account, vote: "downvote")
+    if vote.save
+      redirect_to root_path, notice: "Your downvote has been registered"
+    else
+      existing_vote = @post.votes.find_by(account: current_account)
+      if existing_vote.vote == "downvote"
+        existing_vote.destroy
+        redirect_to root_path, notice: "We've Removed your downvote"
+      else
+        existing_vote.destroy
+        @post.votes.create(account: current_account, vote: "downvote")
+        redirect_to root_path, notice: "Your downvote has been registered"
       end
     end
   end
